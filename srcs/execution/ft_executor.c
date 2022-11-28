eba184e609e08f25c8949800e843e657f03ab4dd
Yoel RIDGWAY-LOPEZ <yridgway@e3r1p14.42.fr>
2022-11-29 00:47:49 +0100

    fixed up the displaying of my cmdtable

--- srcs/execution/ft_executor.c
+++ srcs/execution/ft_executor.c
@@ -15,9 +15,9 @@ void	ft_make_cmd_array(t_data *data, t_cmdline *cmdline, int count)
 	}
 	cmd[i] = NULL;
 	ft_tableadd_back(&data->cmdtable, ft_tablenew(cmd, NULL));
-	i = 0;
-	while (data->cmdtable->cmd[i])
-		printf("cmd1: %s\n", data->cmdtable->cmd[i++]);
+	// i = 0;
+	// while (data->cmdtable->next->cmd[i])
+	// 	printf("cmd1: %s\n", data->cmdtable->next->cmd[i++]);
 }
 
 void	ft_make_operator(t_data *data, char *operator)
@@ -45,25 +45,11 @@ void	make_cmdtable(t_data *data)//, t_cmdline *cmdline, t_cmdtable *cmdtable)
 		{
 			ft_make_cmd_array(data, temp, count);
 		}
-		while (data->cmd && data->cmd->type == PIPE)
+		while (data->cmd && data->cmd->type != WORD && data->cmd->type != NEWLINE)
 		{
-			printf("content: %s\n", data->cmd->content);
 			ft_make_operator(data, data->cmd->content);
 			data->cmd = data->cmd->next;
 		}
-		// cmdline = cmdline->next;
-		// if (cmdline->type == WORD)
-		// {
-		// 	ft_append_cmd(cmdtable, cmdline->content);
-		// 	cmdline = cmdline->next;
-		// }
-		// if (cmdline->type == PIPE)
-		// {
-		// 	cmdtable->next = ft_tablenew();
-		// 	cmdtable = cmdtable->next;
-
-		// }
-		//data->cmd = data->cmd->next;
 	}
 }
 
@@ -72,30 +58,27 @@ int	ft_executor(t_data *data)
 	int	i;
 	// t_cmdtable	*temp;
 
-	data->cmdtable = NULL;
+	data->cmdtable = NULL;//ft_tablenew(NULL, NULL);
 	make_cmdtable(data);
 	while (data->cmdtable)
 	{
 		if (data->cmdtable->operator)
-			printf("operator: %s\n", data->cmdtable->operator);
-		else
+		{
+			ft_putstr_fd(data->cmdtable->operator, 2);
+			write(2, " ", 1);
+		}
+		else if (data->cmdtable->cmd)
 		{
 			i = 0;
+			//write(2, "[", 1);
 			while (data->cmdtable->cmd[i])
-				printf("cmd[%d]: %s\n", i, data->cmdtable->cmd[i++]);
+			{
+				ft_putstr_fd(data->cmdtable->cmd[i++], 2);
+				write(2, " ", 1);
+			}
+			//write(2, "]", 1);
 		}
 		data->cmdtable = data->cmdtable->next;
 	}
-	while (data->cmdtable)
-	{
-		if (data->cmdtable->operator)
-			printf("operator: %s\n", data->cmdtable->operator);
-		else
-		{
-			i = 0;
-			while (data->cmdtable->cmd[i])
-				printf("cmd[%d]: %s\n", i, data->cmdtable->cmd[i++]);
-		}
-		data->cmdtable = data->cmdtable->next;
-	}
+	write(2, "\n", 1);
 }
